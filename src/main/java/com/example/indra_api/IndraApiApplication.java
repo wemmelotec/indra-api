39ef830f7b9cd415905abf14fb18ab10e8c5a87e
wellington <tonmelo@hotmail.com>
2024-06-21 14:17:04 -0300

    criação do projeto e conf h2 e git

---
+++ src/main/java/com/example/indra_api/IndraApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.indra_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/indra_api/IndraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.indra_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
