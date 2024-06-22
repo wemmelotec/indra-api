8636da9bc69733823cf328828809243e30c9cf56
Wellington <tonmelo@hotmail.com>
2024-06-21 21:49:28 -0300

    implementei criar e buscar cliente por id

--- src/main/java/com/example/indra_api/resource/ClienteResource.java
+++ src/main/java/com/example/indra_api/resource/ClienteResource.java
@@ -2,12 +2,18 @@ package com.example.indra_api.resource;
 
 import com.example.indra_api.model.Cliente;
 import com.example.indra_api.repository.ClienteRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+
+import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentRequestUri;
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -17,7 +23,23 @@ public class ClienteResource {
     private ClienteRepository clienteRepository;
 
     @GetMapping
-    public List<Cliente> listar(){
+    public List<Cliente> listar() {
         return clienteRepository.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Cliente buscarPeloId(@PathVariable Long id){
+        return clienteRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cliente não encontrado"));
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> criar(@RequestBody Cliente cliente, HttpServletResponse response) {
+        Cliente clienteSalvo = clienteRepository.save(cliente);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(clienteSalvo.getId()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(clienteSalvo);
     }
 }
