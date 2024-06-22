a10b9ce62a5e7e22b96cdf4c5ba2e2f21678b434
Wellington <tonmelo@hotmail.com>
2024-06-22 16:25:40 -0300

    coloquei as regras de negócio na classe service

--- src/main/java/com/example/indra_api/resource/ClienteResource.java
+++ src/main/java/com/example/indra_api/resource/ClienteResource.java
@@ -1,8 +1,7 @@
 package com.example.indra_api.resource;
 
 import com.example.indra_api.model.Cliente;
-import com.example.indra_api.repository.BeneficioRepository;
-import com.example.indra_api.repository.ClienteRepository;
+import com.example.indra_api.service.ClienteService;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
@@ -11,56 +10,42 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.List;
-
-import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentRequestUri;
 
 @RestController
 @RequestMapping("/api/clientes")
 public class ClienteResource {
 
-    @Autowired
-    private ClienteRepository clienteRepository;
 
     @Autowired
-    private BeneficioRepository beneficioRepository;
+    private ClienteService clienteService;
+
 
     @GetMapping
     public List<Cliente> listar() {
-        return clienteRepository.findAll();
+        return clienteService.listar();
     }
 
     @GetMapping(path = "/{id}")
     public Cliente buscarPeloId(@PathVariable Long id) {
-        return clienteRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cliente não encontrado"));
+        return clienteService.buscarPeloId(id);
     }
 
     @PostMapping
     public ResponseEntity<Cliente> criar(@Valid @RequestBody Cliente cliente, HttpServletResponse response) {
-        Cliente clienteSalvo = clienteRepository.save(cliente);
-
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
-                .buildAndExpand(clienteSalvo.getId()).toUri();
-        response.setHeader("Location", uri.toASCIIString());
-
-        return ResponseEntity.created(uri).body(clienteSalvo);
+        return clienteService.criar(cliente, response);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void remover(@PathVariable Long id) {
-        //beneficioRepository.deleteById(id);
-        clienteRepository.deleteById(id);
+        clienteService.remover(id);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @Valid @RequestBody Cliente cliente) {
-        Cliente clienteSalvo = clienteRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cliente não encontrado"));
-        BeanUtils.copyProperties(cliente, clienteSalvo, "id");
-        clienteRepository.save(clienteSalvo);
-        return ResponseEntity.ok(clienteSalvo);
+
+        return clienteService.atualizar(id,cliente);
     }
 }
---
+++ src/main/java/com/example/indra_api/service/ClienteService.java
@@ -0,0 +1,54 @@
+package com.example.indra_api.service;
+
+import com.example.indra_api.model.Cliente;
+import com.example.indra_api.repository.ClienteRepository;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public List<Cliente> listar() {
+        return clienteRepository.findAll();
+    }
+
+    public Cliente buscarPeloId(@PathVariable Long id) {
+        return clienteRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cliente não encontrado"));
+    }
+
+    public ResponseEntity<Cliente> criar(@Valid @RequestBody Cliente cliente, HttpServletResponse response) {
+        Cliente clienteSalvo = clienteRepository.save(cliente);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(clienteSalvo.getId()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(clienteSalvo);
+    }
+
+    public void remover(@PathVariable Long id) {
+        clienteRepository.deleteById(id);
+    }
+
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @Valid @RequestBody Cliente cliente) {
+        Cliente clienteSalvo = clienteRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cliente não encontrado"));
+        BeanUtils.copyProperties(cliente, clienteSalvo, "id");
+        clienteRepository.save(clienteSalvo);
+        return ResponseEntity.ok(clienteSalvo);
+    }
+}
