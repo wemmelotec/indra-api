dd8154748cbb4aa3c112595556b6661174fb064a
Wellington <tonmelo@hotmail.com>
2024-06-23 12:50:23 -0300

    implementei o swagger com todos os testes a parti dele

--- src/main/java/com/example/indra_api/resource/BeneficioResource.java
+++ src/main/java/com/example/indra_api/resource/BeneficioResource.java
@@ -5,6 +5,7 @@ import com.example.indra_api.model.Beneficio;
 import com.example.indra_api.model.Cliente;
 import com.example.indra_api.service.BeneficioService;
 import com.example.indra_api.service.ClienteService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,6 +14,16 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * <h1>Expõe os EndPoints da classe Benefícios.</h1>
+ * <h2>Métodos:</h2>
+ * <ol>
+ *     <li>buscarPeloId -> recebe o id do benefício no path e retorna o benefício.</li>
+ *     <li>listaTodosOsBeneficiosPorCliente -> recebe o id do cliente no path e retorna todos os beneficios associados ao cliente.</li>
+ *     <li>adicionarBeneficio -> recebe o id do cliente e um benefício e retorna o cliente.</li>
+ *     <li>remover -> recebe o id do benefício no path, não tem retorno e responde com o @ResponseStatus.</li>
+ * </ol>
+ */
 @RestController
 @RequestMapping("/api/beneficios")
 public class BeneficioResource {
@@ -22,22 +33,25 @@ public class BeneficioResource {
     @Autowired
     private ClienteService clienteService;
 
+    @Operation(summary = "Retorna o registro de um único benefício cadastrado.")
     @GetMapping("/{id}")
-    public BeneficioDTOResponse buscarPeloId(@PathVariable Long id){
+    public BeneficioDTOResponse buscarPeloId(@PathVariable Long id) {
         return beneficioService.buscarPeloId(id);
     }
 
+    @Operation(summary = "Retorna os registros dos benefícios por cliente.")
     @GetMapping("/{id}/beneficios")
-    public ResponseEntity<List<BeneficioDTOResponse>> listaTodosOsBeneficiosPorCliente(@PathVariable Long id){
-        //Cliente cliente = clienteService.buscarPeloId(id);
+    public ResponseEntity<List<BeneficioDTOResponse>> listaTodosOsBeneficiosPorCliente(@PathVariable Long id) {
         return beneficioService.listaTodosOsBeneficiosPorCliente(id);//ResponseEntity.ok(cliente.getBeneficios());
     }
 
+    @Operation(summary = "Adiciona o registro de um único benefício a um cliente já cadastrado.")
     @PostMapping("/{id}/beneficios")
-    public ResponseEntity<Cliente> adicionarBeneficio(@PathVariable Long id, @RequestBody Beneficio beneficio, HttpServletResponse response){
+    public ResponseEntity<Cliente> adicionarBeneficio(@PathVariable Long id, @RequestBody Beneficio beneficio, HttpServletResponse response) {
         return beneficioService.adicionarBeneficio(id, beneficio, response);
     }
 
+    @Operation(summary = "Remove o registro de um único benefício.")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void remover(@PathVariable Long id) {
--- src/main/java/com/example/indra_api/resource/ClienteResource.java
+++ src/main/java/com/example/indra_api/resource/ClienteResource.java
@@ -2,6 +2,7 @@ package com.example.indra_api.resource;
 
 import com.example.indra_api.model.Cliente;
 import com.example.indra_api.service.ClienteService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,36 +12,50 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * <h1>Expõe os EndPoints da classe Cliente.</h1>
+ * <h2>Métodos:</h2>
+ * <ol>
+ *     <li>listar -> não recebe nenhum parâmatro e retorna uma lista de clientes.</li>
+ *     <li>buscarPeloId -> recebe o id do cliente no path e retorna o cliente solicitado.</li>
+ *     <li>criar -> recebe um cliente, faz as validação @NotNull no objeto cliente que recebeu e retorna o cliente que foi criado.</li>
+ *     <li>remover -> recebe o id do cliente no path, não tem retorno e responde com o @ResponseStatus.</li>
+ *     <li>atualizar -> recebe o id do cliente no path, recebe o cliente com os atributos atualizados, faz as validação @NotNull e retorna o cliente atualizado.</li>
+ * </ol>
+ */
 @RestController
 @RequestMapping("/api/clientes")
 public class ClienteResource {
 
-
     @Autowired
     private ClienteService clienteService;
 
-
+    @Operation(summary = "Retorna os registros dos clientes cadastrados.")
     @GetMapping
     public List<Cliente> listar() {
         return clienteService.listar();
     }
 
+    @Operation(summary = "Retorna o registro de um único cliente cadastrado.")
     @GetMapping(path = "/{id}")
     public Cliente buscarPeloId(@PathVariable Long id) {
         return clienteService.buscarPeloId(id);
     }
 
+    @Operation(summary = "Salva o registro de um único cliente.")
     @PostMapping
     public ResponseEntity<Cliente> criar(@Valid @RequestBody Cliente cliente, HttpServletResponse response) {
         return clienteService.criar(cliente, response);
     }
 
+    @Operation(summary = "Remove o registro de um único cliente.")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void remover(@PathVariable Long id) {
         clienteService.remover(id);
     }
 
+    @Operation(summary = "Atualiza o registro de um único cliente")
     @PutMapping("/{id}")
     public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @Valid @RequestBody Cliente cliente) {
         return clienteService.atualizar(id, cliente);
